0fa52da4857882b738902b9a6357e862ec74dffb
mauricetjmurphy <mauricetjmurphy@gmail.com>
2023-02-20 22:54:40 +0700

    Optimized images

--- src/features/home/components/LatestArticles/BlogCard.tsx
+++ src/features/home/components/LatestArticles/BlogCard.tsx
@@ -31,7 +31,12 @@ export const BlogCard: React.FC<BlogCardProps> = (props) => {
     >
       <Box>
         <img
-          src={`/images/${props.image_url}`}
+          srcSet={`/images/${props.image_url?.split(".")[0]}-small.jpg 640w,
+          /images/${props.image_url?.split(".")[0]}-medium.jpg 960w, 
+          /images/${props.image_url?.split(".")[0]}-large.jpg 1280w, 
+          /images/${props.image_url?.split(".")[0]}-xl.jpg 1920w`}
+          sizes="(max-width: 600px) 640px, (max-width: 960px) 960px, (max-width: 1280px) 1280px, 1920px"
+          src={`/images/${props.image_url?.split(".")[0]}-xl.jpg`}
           alt={props.title}
           style={{ width: "100%" }}
         />
--- src/features/home/components/LatestArticles/HeadlineCard.tsx
+++ src/features/home/components/LatestArticles/HeadlineCard.tsx
@@ -38,7 +38,12 @@ export const HeadlineCard: React.FC<HeadlineCardProps> = (props) => {
     >
       <Box sx={{ width: "100%" }}>
         <img
-          src={`/images/${props.image_url}`}
+          srcSet={`/images/${props.image_url?.split(".")[0]}-small.jpg 640w,
+          /images/${props.image_url?.split(".")[0]}-medium.jpg 960w, 
+          /images/${props.image_url?.split(".")[0]}-large.jpg 1280w, 
+          /images/${props.image_url?.split(".")[0]}-xl.jpg 1920w`}
+          sizes="(max-width: 600px) 640px, (max-width: 960px) 960px, (max-width: 1280px) 1280px, 1920px"
+          src={`/images/${props.image_url?.split(".")[0]}-xl.jpg`}
           alt={props.title}
           style={{ width: "100%" }}
         />
